d1a02f8556b2806976400699d8f6a8ae21c2ca70
Jain Kunjan <kunjan.j@wellnest.tech>
2024-02-08 16:52:33 +0530

    Added Required Dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("org.jetbrains.kotlin.kapt")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -33,6 +37,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        dataBinding = true
+    }
+
 }
 
 dependencies {
@@ -44,4 +53,32 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-compiler:2.50")
+
+    // Navigation Components
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.5.0")
+
+    // Room Dependency.
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
+    //Coil Dependency.
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    // Lottie Animation Dependency.
+    implementation("com.airbnb.android:lottie:6.0.0")
+
+    implementation ("com.facebook.shimmer:shimmer:0.5.0")
+    implementation ("com.todkars:shimmer-recyclerview:0.4.1")
+
+    implementation("org.jsoup:jsoup:1.10.3")
 }
