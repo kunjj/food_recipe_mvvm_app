ac0889ef43aebe79a344b006cad359cb598791e2
Jain Kunjan <kunjan.j@wellnest.tech>
2024-02-12 19:11:10 +0530

    Made Design Changes In Bubble Navigation.

--- app/src/main/java/com/example/foodrecipesapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipesapplication/ui/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.foodrecipesapplication.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.foodrecipesapplication.R
 import com.example.foodrecipesapplication.databinding.ActivityMainBinding
@@ -11,6 +14,7 @@ import com.example.foodrecipesapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
@@ -18,6 +22,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        binding.bottomNavigationView.setupWithNavController(binding.newsNavHostFragment.findNavController())
+//        binding.bottomNavigationView.setupWithNavController(binding.newsNavHostFragment.findNavController())
+        navController = Navigation.findNavController(this, R.id.navHostFragment)
+
+        binding.bottomNavigationView.addBubbleListener { id ->
+            when (id) {
+                R.id.recipeFragment -> navController.navigate(R.id.recipeFragment)
+                R.id.favoriteRecipeFragment -> navController.navigate(R.id.favoriteRecipeFragment)
+                R.id.foodJokesFragment -> navController.navigate(R.id.foodJokesFragment)
+            }
+        }
     }
 }
