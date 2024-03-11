e0fc3a8bc7987ee4b53d42025e938d89bb0ca537
Jain Kunjan <kunjan.j@wellnest.tech>
2024-03-11 18:00:46 +0530

    Made Data Class Parcelable.

--- app/src/main/java/com/example/foodrecipesapplication/models/Us.kt
+++ app/src/main/java/com/example/foodrecipesapplication/models/Us.kt
@@ -1,8 +1,11 @@
 package com.example.foodrecipesapplication.models
 
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Us(
     @SerializedName("amount")
     val amount: Double,
@@ -10,4 +13,4 @@ data class Us(
     val unitLong: String,
     @SerializedName("unitShort")
     val unitShort: String
-)
+) : Parcelable
