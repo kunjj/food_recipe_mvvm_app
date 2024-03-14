902ffcbe43bbbf953fad3c4f99fd9838ea7e4f8f
Jain Kunjan <kunjan.j@wellnest.tech>
2024-03-14 18:18:55 +0530

    Made A Minnor Change.

--- app/src/main/java/com/example/foodrecipesapplication/ui/fragments/RecipesFilterFragment.kt
+++ app/src/main/java/com/example/foodrecipesapplication/ui/fragments/RecipesFilterFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.foodrecipesapplication.databinding.FragmentRecipesFilterBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class RecipesFilterFragment : Fragment() {
+class RecipesFilterFragment : BottomSheetDialogFragment() {
     private lateinit var binding: FragmentRecipesFilterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
