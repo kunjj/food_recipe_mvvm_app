aeb2938b67e289db916d87bb327f344223a495b4
Jain Kunjan <kunjan.j@wellnest.tech>
2024-05-06 18:01:54 +0530

    Completed Design Of Food Joke Fragment.

--- app/src/main/java/com/example/foodrecipesapplication/ui/fragments/FoodJokesFragment.kt
+++ app/src/main/java/com/example/foodrecipesapplication/ui/fragments/FoodJokesFragment.kt
@@ -1,18 +1,20 @@
 package com.example.foodrecipesapplication.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.foodrecipesapplication.R
+import androidx.fragment.app.Fragment
+import com.example.foodrecipesapplication.databinding.FragmentFoodJokesBinding
 
 class FoodJokesFragment : Fragment() {
+    private var binding: FragmentFoodJokesBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_food_jokes, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        this.binding = FragmentFoodJokesBinding.inflate(inflater, container, false)
+        return this.binding!!.root
     }
 }
